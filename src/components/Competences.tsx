bfe55a322b4a8a303060033dd48efb37cf4b3324
Mady Seta <ralaimadyseta@gmail.com>
2026-02-14 00:13:11 +0800

    update deploy

--- src/components/Competences.tsx
+++ src/components/Competences.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import CompetenceCard from "./CompetenceCard";
 import ProjectTitle from "./ProjectTitle";
 
